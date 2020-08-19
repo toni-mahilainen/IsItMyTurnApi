c4f1b84834d9d6eb3fd4d35d0f6a2db01038eeef
Toni Mahilainen <toni.mahilainen@student.careeria.fi>
2020-08-19 15:54:40 +0300

    Minor fixes

--- Controllers/ApartmentController.cs
+++ Controllers/ApartmentController.cs
@@ -60,7 +60,7 @@ namespace IsItMyTurnApi.Controllers
                 int currentApartmentId;
 
                 // If the apartment ID is 5, next in shift will be an apartment 1
-                if (lastApartmentId < 5)
+                if (lastApartmentId < 6)
                 {
                     currentApartmentId = lastApartmentId + 1;
                 }
